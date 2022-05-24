747e644c68d1e8089fc0434488ebc15e1a1cc43c
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-05-24 11:11:45 +0100

    card title spacing

--- src/components/SocialLogin.js
+++ src/components/SocialLogin.js
@@ -1,13 +1,21 @@
 export default function SocialLogin({ action }) {
   return (
     <div className={`alt-login ${action}`}>
-      {action === "Login" && <h3 className="card-title">or login with...</h3>}
-      {action === "Join" && <h3 className="card-title">join with...</h3>}
+      {action === "Login" && (
+        <h3 className="card-title" id="login-social">
+          or login with...
+        </h3>
+      )}
+      {action === "Join" && (
+        <h3 className="card-title" id="join-social">
+          join with...
+        </h3>
+      )}
 
       <div className="socials">
-        <div className="fb-login social-button">
-          <i className="fa-brands fa-facebook-f"></i>
-          <a href="">{action} with Facebook</a>
+        <div className="li-login social-button ">
+          <i className="fa-brands fa-linkedin-in"></i>
+          <a href="">{action} with LinkedIn</a>
           <div className="spacer"></div>
         </div>
 
@@ -19,10 +27,9 @@ export default function SocialLogin({ action }) {
           <a href="">{action} with Google</a>
           <div className="spacer"></div>
         </div>
-
-        <div className="li-login social-button ">
-          <i className="fa-brands fa-linkedin-in"></i>
-          <a href="">{action} with LinkedIn</a>
+        <div className="fb-login social-button">
+          <i className="fa-brands fa-facebook-f"></i>
+          <a href="">{action} with Facebook</a>
           <div className="spacer"></div>
         </div>
       </div>
